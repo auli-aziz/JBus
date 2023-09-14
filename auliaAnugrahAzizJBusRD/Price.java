bf25436c984cd265f5699898bd487a5d8478a1b9
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2023-09-14 21:59:27 +0700

    CS02 - Menambahkan Price dan Rating

---
+++ auliaAnugrahAzizJBusRD/Price.java
@@ -0,0 +1,33 @@
+package auliaAnugrahAzizJBusRD;
+
+
+/**
+ * Write a description of class Price here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Price
+{
+    public double rebate;
+    public double price;
+    public int discount;
+    
+    public Price(double price){
+        this.price = price;
+        this.discount = 0;
+        this.rebate = 0;
+    }
+    
+    public Price(double price, int discount){
+        this.price = price;
+        this.discount = discount;
+        this.rebate = 0;
+    }
+    
+    public Price(double price, double rebate){
+        this.price = price;
+        this.discount = 0;
+        this.rebate = rebate;
+    }
+}
---
+++ auliaAnugrahAzizJBusRD/Rating.java
@@ -0,0 +1,19 @@
+package auliaAnugrahAzizJBusRD;
+
+
+/**
+ * Write a description of class Rating here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Rating
+{
+    private long count;
+    private long total;
+    
+    Rating() {
+        this.count = 0;
+        this.total = 0;
+    }
+}
