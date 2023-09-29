1a3764a121e823bf40a724dff65f20a6e54db3dc
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2023-09-29 11:42:41 +0700

    CS04 - memperbaiki parameter Bus

--- auliaAnugrahAzizJBusRD/Bus.java
+++ auliaAnugrahAzizJBusRD/Bus.java
@@ -23,7 +23,7 @@ public class Bus extends Serializable implements FileParser
     public City city;
     public List<Schedule> schedules;
     
-    public Bus(int id, String name, Facility facility, Price price, int capacity, BusType busType, City city, Station departure, Station arrival, List<Schedule> schedules) {
+    public Bus(int id, String name, Facility facility, Price price, int capacity, BusType busType, City city, Station departure, Station arrival) {
         super(id);
         this.capacity = capacity;
         this.facility = facility;
--- auliaAnugrahAzizJBusRD/JBus.java
+++ auliaAnugrahAzizJBusRD/JBus.java
@@ -1,4 +1,5 @@
 package auliaAnugrahAzizJBusRD;
+import java.util.Calendar;
 
 
 /**
@@ -22,10 +23,28 @@ public class JBus
             System.out.println(price.price);    
         }
         
-        
+        System.out.println("Below 12000.0");
+        System.out.println(Validate.filter(unfilteredArray, 12000, true));
+        System.out.println(Validate.filter(unfilteredArray, 10000, false));
+        Bus testBus=createBus(); 
+        Payment testPayment=new Payment(1,1,1,testBus.id,"S1"); 
+        System.out.println(testPayment.getDepartureInfo()); 
+        System.out.println(testPayment.getTime()); 
+        Calendar schedule1=Calendar.getInstance(); 
+        testBus.addSchedule(schedule1); 
+        Calendar schedule2=Calendar.getInstance(); 
+        schedule2.add(Calendar.DAY_OF_MONTH,3); 
+        testBus.addSchedule(schedule2); 
+        for(Schedule s: testBus.schedules){ 
+            testBus.printSchedule(s); 
+        }
     }
     
-    
+    public static Bus createBus() {
+        Price price = new Price(750000, 5);
+        Bus bus = new Bus(1, "Netlab Bus", Facility.LUNCH, price, 25, BusType.REGULER, City.BANDUNG, new Station(1, "Depok Terminal", City.DEPOK, "Jl. Margonda Raya"), new Station(2, "Halte UI", City.JAKARTA, "Universitas Indonesia"));
+        return bus;
+    }
 
     public static int getBusId() {
         return 0;
